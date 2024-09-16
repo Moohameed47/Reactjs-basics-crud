fc8045f03dbf33cff842f975dcb2d580efcd99a3
Mohamed Tarek <d.mohamedtarek12@gmail.com>
2024-09-16 05:36:18 +0300

    update on ProductCard UI

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -10,24 +10,24 @@ const ProductCard = ({ product }: IProps) => {
   const { title, description, imageURL, price, category } = product;
   return (
     <>
-      <div className="max-w-sm md:max-w-lg mx-auto md:mx-0 border rounded-md p-2 flex flex-col">
+      <div className="max-w-sm md:max-w-lg mx-auto md:mx-0 border rounded-md p-2 flex flex-col space-y-3">
         <Image imageURL={imageURL} alt={title} className="rounded-md h-52 w-full lg:object-cover mb-2" />
 
-        <h3>{title}</h3>
-        <p>{textSlicer(description)}</p>
+        <h3 className="text-lg font-semibold">{textSlicer(title, 25)}</h3>
+        <p className="text-xs text-gray-500 break-words">{textSlicer(description)}</p>
 
-        <div className="flex items-center my-4 space-x-2">
+        <div className="flex items-center space-x-2">
           <span className="w-5 h-5 bg-indigo-600 rounded-full cursor-pointer" />
           <span className="w-5 h-5 bg-yellow-600 rounded-full cursor-pointer" />
           <span className="w-5 h-5 bg-red-600 rounded-full cursor-pointer" />
         </div>
 
         <div className="flex justify-between items-center">
-          <span>${price}</span>
-          <Image imageURL={category.imageURL} alt={category.name} className="w-10 h-10 rounded-full object-center" />
+          <span className="text-lg text-indigo-600 font-semibold">${price}</span>
+          <Image imageURL={category.imageURL} alt={category.name} className="w-10 h-10 rounded-full object-bottom" />
         </div>
 
-        <div className="flex justify-between items-center space-x-2 mt-5">
+        <div className="flex justify-between items-center space-x-2">
           <Button
             className="bg-indigo-700"
             width="w-full"
