df7b6e3fcd5cc89d181f61c079b1cbe6d3c4fc21
Mohamed Tarek <d.mohamedtarek12@gmail.com>
2024-09-18 03:54:09 +0300

    Enhance add product modal form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import ProductCard from "./components/ProductCard/ProductCard";
-import Modal from "./components/UI/Button/Modal/Modal";
-import { productList } from "./Data";
+import Modal from "./components/UI/Modal/Modal";
+import { formInputsList, productList } from "./Data";
 import Button from "./components/UI/Button/Button";
+import Input from "./components/UI/Input/Input";
 function App() {
   /* ــــــــSTATEــــــــ */
   const [isOpen, setIsOpen] = useState(false);
@@ -19,6 +20,14 @@ function App() {
 
   /* ــــــــRENDERــــــــ */
   const renderProductList = productList.map((product) => <ProductCard product={product} key={product.id} />);
+  const renderAllInputList = formInputsList.map((input) => (
+    <div className="flex flex-col">
+      <label className="text-sm font-medium text-gray-700 mb-[1px]" htmlFor={input.id}>
+        {input.label}
+      </label>
+      <Input type="text" id={input.id} name={input.name} />
+    </div>
+  ));
 
   return (
     <>
@@ -29,14 +38,17 @@ function App() {
         <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 rounded-md">{renderProductList}</div>
       </main>
       <Modal isOpen={isOpen} closeModal={close} title="ADD A NEW PRODUCT">
-        <div className="flex items-center space-x-3">
-          <Button className="bg-indigo-700 hover:bg-indigo-800" width="w-full">
-            Submit
-          </Button>
-          <Button className="bg-gray-300 hover:bg-gray-400" width="w-full">
-            Cancel
-          </Button>
-        </div>
+        <form className="space-y-3">
+          {renderAllInputList}
+          <div className="flex items-center space-x-3">
+            <Button className="bg-indigo-700 hover:bg-indigo-800" width="w-full">
+              Submit
+            </Button>
+            <Button className="bg-gray-400 hover:bg-gray-500" width="w-full">
+              Cancel
+            </Button>
+          </div>
+        </form>
       </Modal>
     </>
   );
